e2f4d6957653d42260a7c5293bceaf9f909d3f87
Marcos Wilson <mwilson.oliveira@gmail.com>
2023-10-18 20:42:05 -0300

    Style the issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,8 @@
 //Os ids são strings porque nas URLs eles são
 
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 
 //sempre strings que futuramente são passados para number
@@ -9,10 +11,6 @@ interface Props {
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
-  //Caso o usuário digite algo que não seja um número na rota
-  //ele é redirecionado para a página 404
-  if (typeof params.id !== "number") notFound();
-
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
@@ -21,10 +19,15 @@ const IssueDetailPage = async ({ params }: Props) => {
 
   return (
     <div>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+      <Heading>{issue.title}</Heading>
+      <Flex className="space-x-3" my="2">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+
+      <Card>
+        <p>{issue.description}</p>
+      </Card>
     </div>
   );
 };
