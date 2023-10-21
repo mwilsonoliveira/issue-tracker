99324b96748702108e8267becc0f8315c2016546
Marcos Wilson <mwilson.oliveira@gmail.com>
2023-10-21 20:26:34 -0300

    Handle errors

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -8,43 +8,67 @@ import {
 } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
+
+  const deleteIssue = async () => {
+    try {
+      axios.delete("/api/issues/" + issueId);
+      router.push("/issues");
+      //necessário fazer um refresh para limpar o cache do Next
+      router.refresh();
+    } catch (error) {
+      setError(true);
+    }
+  };
 
   return (
-    <AlertDialog.Root>
-      <AlertDialogTrigger>
-        <Button color="red">Deletar Issue</Button>
-      </AlertDialogTrigger>
-      <AlertDialog.Content>
-        <AlertDialog.Title>Confirmar</AlertDialog.Title>
-        <AlertDialog.Description>
-          Tem certeza que você quer deletar essa issue? Essa ação não pode ser
-          desfeita.
-        </AlertDialog.Description>
-        <Flex mt="4" gap="3">
-          <AlertDialog.Cancel>
-            <Button variant="soft" color="gray">
-              Cancelar
-            </Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button
-              color="red"
-              onClick={async () => {
-                axios.delete("/api/issues/" + issueId);
-                router.push("/issues");
-                //necessário fazer um refresh para limpar o cache do Next
-                router.refresh();
-              }}
-            >
-              Deletar Issue
-            </Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+    <>
+      <AlertDialog.Root>
+        <AlertDialogTrigger>
+          <Button color="red">Deletar Issue</Button>
+        </AlertDialogTrigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirmar</AlertDialog.Title>
+          <AlertDialog.Description>
+            Tem certeza que você quer deletar essa issue? Essa ação não pode ser
+            desfeita.
+          </AlertDialog.Description>
+          <Flex mt="4" gap="3">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancelar
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color="red" onClick={deleteIssue}>
+                Deletar Issue
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Erro</AlertDialog.Title>
+          <AlertDialog.Description>
+            Essa issue não pode ser deletada!
+          </AlertDialog.Description>
+          <Button
+            color="gray"
+            variant="soft"
+            mt="2"
+            onClick={() => setError(false)}
+          >
+            Ok
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
